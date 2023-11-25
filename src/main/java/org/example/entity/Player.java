7c339c307e7d6aad58508eff74f0dd57525ec28f
meriame <meriamezaouia9999@gmail.com>
2023-11-25 13:34:14 +0100

    scaling playing images

--- src/main/java/org/example/entity/Player.java
+++ src/main/java/org/example/entity/Player.java
@@ -2,7 +2,9 @@ package org.example.entity;
 
 import org.example.GamePanel;
 import org.example.KeyHandler;
+import org.example.UtilityTool;
 
+import javax.imageio.IIOException;
 import javax.imageio.ImageIO;
 import java.awt.*;
 import java.awt.image.BufferedImage;
@@ -39,20 +41,31 @@ public class Player extends Entity{
     }
     public void getPlayerImages(){
 
-        try {
-            up1 = ImageIO.read(getClass().getResource("/player/boy_up_1.png"));
-            up2 = ImageIO.read(getClass().getResource("/player/boy_up_2.png"));
-            down1 = ImageIO.read(getClass().getResource("/player/boy_down_1.png"));
-            down2 = ImageIO.read(getClass().getResource("/player/boy_down_2.png"));
-            left1 = ImageIO.read(getClass().getResource("/player/boy_left_1.png"));
-            left2 = ImageIO.read(getClass().getResource("/player/boy_left_2.png"));
-            right1 = ImageIO.read(getClass().getResource("/player/boy_right_1.png"));
-            right2 = ImageIO.read(getClass().getResource("/player/boy_right_2.png"));
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        up1 = setup("boy_up_1");
+        up2 = setup("boy_up_2");
+        down1 = setup("boy_down_1");
+        down2 = setup("boy_down_2");
+        left1 = setup("boy_left_1");
+        left2 = setup("boy_left_2");
+        right1 = setup("boy_right_1");
+        right2 = setup("boy_right_2");
 
     }
+
+    public BufferedImage setup(String imageName){
+
+        UtilityTool uTool = new UtilityTool();
+        BufferedImage image = null;
+
+        try{
+           image = ImageIO.read(getClass().getResource("/player/"+imageName+".png"));
+            image =uTool.scaleImage(image,gp.tileSize, gp.tileSize);
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return image;
+    }
+
     public void update(){
         if(kh.leftPressed || kh.downPressed || kh.upPressed || kh.rightPressed){
             if(kh.upPressed){ direction="up" ;worldY -= speed;;}
@@ -175,6 +188,6 @@ public class Player extends Entity{
                 break;
 
         }
-        g.drawImage(image, screenX, screenY, gp.tileSize, gp.tileSize, null);
+        g.drawImage(image, screenX, screenY,null);
     }
 }
